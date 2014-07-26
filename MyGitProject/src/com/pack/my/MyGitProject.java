38d58aa102f6fe3658635b169fe87fe6b49ab205
Aamir Ikram <amirikram805@eclipseresource.com>
2014-07-26 19:07:45 +0500

    Create Project

---
+++ MyGitProject/src/com/pack/my/MyGitProject.java
@@ -0,0 +1,11 @@
+package com.pack.my;
+
+public class MyGitProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Pakistan");
+
+	}
+
+}
